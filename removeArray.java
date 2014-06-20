403366599c9524feb86528f0181435f7a572ab1f
atlas1017 <greggmoon1017@gmail.com>
2014-06-20 12:17:40 -0500

    Update removeArray.java

--- removeArray.java
+++ removeArray.java
@@ -1,30 +1,18 @@
 public class Solution {
     public int removeDuplicates(int[] A) {
-        
-        if(A.length == 0)
-            return 0;
-        
-        int i, j, count;
-        count = 1;
-        for(i = 0; i < A.length-1; i++)
+      int i,j,count;
+      count = 1;
+      j = 1;
+      if(A.length <= 1)
+        return A.length;
+      for(i = 0; i < A.length-1; i++)
+      {
+        if(A[i+1] != A[i])
         {
-            if(A[i+1] != A[i])
-                count++;
+            A[j++] = A[i+1];
+            count++;
         }
-        
-        
-        int[] res = new int[count];
-        res[0] = A[0];
-        j = 0;
-        for(i = 0; i < A.length; i++)
-        {
-            if(A[i] != res[j])
-            {
-                res[j+1] = A[i];
-                j++;
-            }
-        }
-        A = res;
-        return count;
+      }
+      return count;
     }
 }
